51d60dbe624dfff8e06b42dba3771573909618c2
Maurilio Rodriguez <maurilio.rodriguez@ashfieldhealthcare.com>
2025-02-28 21:17:52 -0500

    creating project SwiftUI

---
+++ LittleLemonApp/LittleLemonApp/LittleLemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Maurilio Rodriguez on 2025-02-28.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
